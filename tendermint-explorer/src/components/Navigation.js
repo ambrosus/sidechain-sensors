3d2b6333705be2cf2e23ec828880c188d711b534
Dmytro Nasyrov <dmytro@pharosproduction.com>
2018-05-03 13:17:25 +0300

    fix layouts

--- tendermint-explorer/src/components/Navigation.js
+++ tendermint-explorer/src/components/Navigation.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Navbar, Button } from 'reactstrap'
 
 import { logout, isLoggedIn } from '../services/TendermintService'
 
-export default () => (
-    <div>
-        { !isLoggedIn() ? <p></p> : <button className="btn log" onClick={ logout }>Log out</button> }
-    </div>
-)
+// Component
+
+export default () => {
+    if (!isLoggedIn()) {
+        return <div></div>
+    }
+
+    return (
+        <div className="header">
+            <Navbar>
+                <Button color="danger" onClick={ logout }>Sign out</Button>
+            </Navbar>  
+        </div> 
+    )
+}
